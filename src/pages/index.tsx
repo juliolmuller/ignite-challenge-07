5ae064ecac92395d76cbea373d574fd599a67561
Julio Muller <juliodolago@gmail.com>
2022-07-14 23:54:31 -0300

    feat: implement infinity query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Button, Box } from '@chakra-ui/react';
 import { useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
@@ -16,14 +17,22 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery('images', async () => {
-    const response = await api.get('/api/images');
-    console.log(response.data);
-    return response.data;
-  });
+  } = useInfiniteQuery(
+    'images',
+    async ({ pageParam }) => {
+      const response = await api.get('/api/images', {
+        params: pageParam && { after: pageParam },
+      });
+
+      return response.data;
+    },
+    {
+      getNextPageParam: (response: any) => response.after,
+    }
+  );
 
   const formattedData = useMemo(() => {
-    return data?.pages[0].data ?? [];
+    return data?.pages.map(page => page.data).flat() ?? [];
   }, [data]);
 
   if (isLoading) {
@@ -40,7 +49,16 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO: RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+
+        {hasNextPage && (
+          <Button
+            disabled={isFetchingNextPage}
+            onClick={() => fetchNextPage()}
+            mt="10"
+          >
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
